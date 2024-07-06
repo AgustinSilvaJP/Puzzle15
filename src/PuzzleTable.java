5ecfe7b664fd7bf819034bc6ca7bb6b511b8d29b
Agus <AgustinSilvaJP@gmail.com>
2024-07-06 18:51:21 -0300

    add nueva funcionalidad de mover arriba, abajo, derecha e izquierda

--- src/PuzzleTable.java
+++ src/PuzzleTable.java
@@ -1,11 +1,11 @@
-
-//╠ ╣ ╦ ╩ ╬
 public class PuzzleTable {
     private int[][] gameTable;
     static int cantMoves;
     private int[] emptyPos;
     private int[] adyL;
     private int[] adyR;
+    private int[] adyUp;
+    private int[] adyDown;
 
     public PuzzleTable(){
         gameTable = Utilities.arrayTo2x2Matrix(Utilities.randomLineToIntArray(Utilities.randomInitGame()));
@@ -13,15 +13,38 @@ public class PuzzleTable {
         emptyPos = Utilities.emptyIndex(gameTable);
         adyL = new int[] {emptyPos[0], emptyPos[1] - 1};
         adyR = new int[] {emptyPos[0], emptyPos[1] + 1};
+        adyUp = new int[] {emptyPos[0] - 1, emptyPos[1]};
+        adyDown = new int[] {emptyPos[0] + 1, emptyPos[1]};
     }
-
+    public void actAdyThings(){
+        emptyPos = Utilities.emptyIndex(gameTable);
+        adyR = new int[] {emptyPos[0], emptyPos[1] + 1};
+        adyL = new int[] {emptyPos[0], emptyPos[1] - 1};
+        adyUp = new int[] {emptyPos[0] - 1, emptyPos[1]};
+        adyDown = new int[] {emptyPos[0] + 1, emptyPos[1]};
+        cantMoves++;
+    }
+    public void moveDown(){
+        try{
+            gameTable[emptyPos[0]][emptyPos[1]] = gameTable[adyUp[0]][adyUp[1]];
+            gameTable[adyUp[0]][adyUp[1]] = 0;
+            actAdyThings();
+        }catch(Exception e){
+        }
+    }
+    public void moveUp(){
+        try{
+            gameTable[emptyPos[0]][emptyPos[1]] = gameTable[adyDown[0]][adyDown[1]];
+            gameTable[adyDown[0]][adyDown[1]] = 0;
+            actAdyThings();
+        }catch(Exception e){
+        }
+    }
     public void moveL(){
         try{
             gameTable[emptyPos[0]][emptyPos[1]] = gameTable[adyR[0]][adyR[1]];
             gameTable[adyR[0]][adyR[1]] = 0;
-            emptyPos = Utilities.emptyIndex(gameTable);
-            adyR = new int[] {emptyPos[0], emptyPos[1] + 1};
-            adyL = new int[] {emptyPos[0], emptyPos[1] - 1};
+            actAdyThings();
         }catch(Exception e){
         }
     }
@@ -29,9 +52,7 @@ public class PuzzleTable {
         try{
             gameTable[emptyPos[0]][emptyPos[1]] = gameTable[adyL[0]][adyL[1]];
             gameTable[adyL[0]][adyL[1]] = 0;
-            emptyPos = Utilities.emptyIndex(gameTable);
-            adyL = new int[] {emptyPos[0], emptyPos[1] - 1};
-            adyR = new int[] {emptyPos[0], emptyPos[1] + 1};
+            actAdyThings();
         }catch(Exception e){
         }
     }
